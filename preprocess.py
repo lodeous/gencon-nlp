784cd79350d41a873cd1d96e5c7cf023f61d18c4
Caleb Wilson <lodeous@gmail.com>
2021-04-06 15:38:24 -0600

    easy methods for getting the talks in the training set vs the test set

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,61 @@ def get_corpus_and_basic_stats(root):
     corpus = PlainTextCorpusReader(root, r"[0-9]+(?!meta)\.txt")
     print("Creating frequency distribution")
     freqdist = FreqDist(corpus.words())
-
+    
+def discover_train_test_split_files():
+    train_list = []
+    
+    with os.scandir(DATA_ROOT + "train") as it:
+        for entry in it:
+            if entry.is_file():
+                if entry.name.endswith('.meta.txt'):
+                    pass
+                elif entry.name.endswith('.txt'):
+                    train_list.append(entry.name)
+    
+    test_list = []
+    with os.scandir(DATA_ROOT + "test") as it:
+        for entry in it:
+            if entry.is_file():
+                if entry.name.endswith('.meta.txt'):
+                    pass
+                elif entry.name.endswith('.txt'):
+                    test_list.append(entry.name)
+                    
+    with open(DATA_ROOT + "train_split.info", 'w') as f:
+        for filename in train_list:
+            f.write(filename + '\n')
+    with open(DATA_ROOT + "test_split.info", 'w') as f:
+        for filename in test_list:
+            f.write(filename + '\n')
+    
+    return train_list, test_list
+            
+def get_train_test_split():
+    """Returns a list of files in the training split and one of files in the test split."""
+    if not os.path.exists(DATA_ROOT + "train_split.info"):
+        return discover_train_test_split_files()
+    
+    with open(DATA_ROOT + "train_split.info") as f:
+        train_list = [line.strip() for line in f]
+    
+    with open(DATA_ROOT + "test_split.info") as f:
+        test_list = [line.strip() for line in f]
+        
+    return train_list, test_list
+    
+def mark_train_test_in_dataframe(df):
+    """For a Pandas DataFrame with the column "File", this method modifies in place the given
+    DataFrame to have a column "Train" with a value of 1 signalling if the file is in the
+    training set and 0 if its in the test set."""
+    train_list, test_list = get_train_test_split()
+    
+    #Create new column marking the split
+    df["Train"] = 1
+    
+    for filename in test_list:
+        df.loc[df["File"] == filename, "Train"] = 0
+    
 def create_train_test_split(test_ratio=0.3, random_seed=9001):
     summary_df = pd.read_csv(DATA_ROOT+"summary.csv", index_col=0)
     
